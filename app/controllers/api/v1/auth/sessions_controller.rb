eaf11bebcc2db6562a948d9d40c75f9b488d6300
大野幹人 <mbp@oonomikihitonoMacBook-Pro.local>
2022-11-07 13:41:00 +0900

    コメントを修正

--- app/controllers/api/v1/auth/sessions_controller.rb
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Auth::SessionsController < DeviseTokenAuth::SessionsController
   def test_login
-    # ログインしていないテストユーザーか最終ログインが一番遠いユーザーを取得
+    # ログインしたことがないテストログインユーザーか最終ログインが一番遠いテストログインユーザーを取得
     @resource = User.where(test_login: true).order(Arel.sql("current_sign_in_at IS NOT NULL, current_sign_in_at ASC")).first
     Rails.logger.info @resource
 
