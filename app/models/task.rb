bad284c2be2a77c297b02b9e4c04c237a7deda9d
MK8-31 <popofst@yahoo.co.jp>
2022-11-29 15:43:05 +0900

    達成日数や週に何回タスクを達成したかの計算を修正&テスト追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,42 +4,41 @@ class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
 
   def task_record_save(task_record, record)
-    # taskのlast_timeを更新
-    self.update(last_time: task_record.created_at)
-    exp = record.exp
-
     # 経験値を更新
     # タスクを達成したことによる経験値
-    exp = record.exp += 5
+    exp = record.exp + 5
 
-    self.update(day: (Date.today - self.created_at.to_date).to_i)
-
-    week = ((Date.today - self.created_at.to_date) / 7).to_i
-    if week != self.week
-      self.update(week: week)
-
-      # 週を跨いだらリセット
+    # リセット日ならばリセット
+    if is_reset_date(task_record)
       self.update(days_a_week: 0)
     end
 
     # 週に何回タスクを達成したか
-
     self.update(days_a_week: self.days_a_week + 1)
+
+    # 連続してタスクを達成によるボーナス
+    if self.last_time.nil? || (task_record.created_at.to_date - self.last_time.to_date).to_i == 1
+      self.update(running_days: self.running_days + 1)
+      exp += self.running_days * record.level
+    else
+      self.update(running_days: 1)
+    end
+
+    # taskのlast_timeを更新
+    self.update(last_time: task_record.created_at)
+
+    # taskの達成日数を１追加
+    self.update(day: self.day + 1)
 
     # 週に4回以上タスクを行うとボーナス
     exp += record.level * 100 if self.days_a_week >= 4
 
     if self.days_a_week == 4
       self.update(running_weeks: self.running_weeks + 1)
+      self.update(week: self.week + 1)
 
       # 続いている週数によってボーナス
       exp += self.running_weeks * 100
-    end
-
-    # 連続してタスクを達成によるボーナス
-    if (Date.today - self.last_time).to_i == 1
-      self.update(running_days: self.running_days + 1)
-      exp += self.running_days * record.level
     end
 
     previous_level = record.level
@@ -56,5 +55,26 @@ class Task < ApplicationRecord
     Rails.logger.info self
 
     return record, is_level_up
+  end
+
+  def is_reset_date(task_record)
+    # 次のリセット日までの日数
+    days_until_next_reset_date = 7 - ((task_record.created_at.to_date - self.created_at.to_date) % 7)
+
+    # 週の変わり目の場合
+    return true if days_until_next_reset_date == 7
+
+    # 次のリセット日
+    next_reset_date = task_record.created_at.to_date + days_until_next_reset_date
+
+    # 次のリセット日から前回の記録日が７日よりも離れていた場合
+    return true if !self.last_time.nil? && (next_reset_date - self.last_time.to_date) > 7
+
+    Rails.logger.debug task_record.created_at.to_date
+    Rails.logger.debug self.created_at.to_date
+    Rails.logger.debug days_until_next_reset_date
+    Rails.logger.debug next_reset_date
+
+    return false
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -22,14 +22,63 @@ RSpec.describe Task, type: :model do
     expect(task.valid?).to eq false
   end
 
-  it 'task_record_saveが正常に機能しているか' do
-    task = @user.tasks.create(name: 'do hello')
-    task_record = task.task_records.create
-    task.task_record_save(task_record, @record)
-    expect(@record.exp).to eq 5
-    expect(task.last_time).to eq task_record.created_at.to_date
-    expect(task.day).to eq  (Date.today - task.created_at.to_date).to_i
-    expect(task.days_a_week).to eq 1
-    expect(task.running_days).to eq 0
+  describe '#task_record_save' do
+    it 'task_record_saveが正常に機能しているか' do
+      task = @user.tasks.create(name: 'do hello')
+      task_record = task.task_records.create
+      Rails.logger.info @record
+      task.task_record_save(task_record, @record)
+      expect(@record.exp).to eq 6
+      expect(task.last_time).to eq task_record.created_at.to_date
+      expect(task.day).to eq  1
+      expect(task.days_a_week).to eq 1
+      expect(task.running_days).to eq 1
+    end
+
+    it 'days_a_weekが適切にリセットされるか' do
+      task1 = @user.tasks.create(name: 'task1', created_at: 7.days.ago, days_a_week: 4, last_time: 1.days.ago)
+      task2 = @user.tasks.create(name: 'task2', created_at: 8.days.ago, days_a_week: 4, last_time: 2.days.ago)
+      task_record1 = task1.task_records.create
+      task_record2 = task2.task_records.create
+      task1.task_record_save(task_record1, @record)
+      task2.task_record_save(task_record2, @record)
+      expect(task1.days_a_week).to eq 1
+      expect(task2.days_a_week).to eq 1
+    end
+
+    it '同じ週で継続している場合にdays_a_weekがリセットされない' do
+      task = @user.tasks.create(name: 'do hello')
+      7.times do |i|
+        travel_to Date.today + i do
+          task_record = task.task_records.create
+          task.task_record_save(task_record, @record)
+          expect(task.days_a_week).to eq 1+i
+          expect(task.day).to eq  1+i
+          expect(task.running_days).to eq 1+i
+        end
+      end
+    end
+
+    it 'running_daysが正常に稼働しているか' do
+      task1 = @user.tasks.create(name: 'task1', created_at: 7.days.ago, days_a_week: 4, last_time: 1.days.ago, running_days: 6)
+      task2 = @user.tasks.create(name: 'task2', created_at: 8.days.ago, days_a_week: 4, last_time: 2.days.ago, running_days: 6)
+      task_record1 = task1.task_records.create
+      task_record2 = task2.task_records.create
+      task1.task_record_save(task_record1, @record)
+      task2.task_record_save(task_record2, @record)
+      expect(task1.running_days).to eq 7
+      expect(task2.running_days).to eq 1
+    end
+
+    it 'running_weeksが正常に稼働しているか' do
+      task1 = @user.tasks.create(name: 'task1', created_at: 27.days.ago, days_a_week: 3, last_time: 1.days.ago, running_weeks: 2)
+      task2 = @user.tasks.create(name: 'task2', created_at: 27.days.ago, days_a_week: 4, last_time: 2.days.ago, running_weeks: 2)
+      task_record1 = task1.task_records.create
+      task_record2 = task2.task_records.create
+      task1.task_record_save(task_record1, @record)
+      task2.task_record_save(task_record2, @record)
+      expect(task1.running_weeks).to eq 3
+      expect(task2.running_weeks).to eq 2
+    end
   end
 end
